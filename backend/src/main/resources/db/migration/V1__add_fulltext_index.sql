fa32745248dd8a473af9c2f9cae71de818f00aa2
limkanghyun <limkh06910691@gmail.com>
2025-06-06 01:46:56 +0900

    refactor: roomId 인덱스 삭제 - full text index와 복합으로 불가능하므로 삭제

--- backend/src/main/resources/db/migration/V1__add_fulltext_index.sql
+++ backend/src/main/resources/db/migration/V1__add_fulltext_index.sql
@@ -1,7 +1,3 @@
 ALTER TABLE chat_message_search
     ADD FULLTEXT INDEX idx_content_ngram (content)
         WITH PARSER ngram;
-
-ALTER TABLE chat_message_search
-    ADD INDEX idx_room_id (room_id);
-
